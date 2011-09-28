4fa9d9e245c6619e380d33cabbddb13fcf780329
Joaquín Rosales <globojorro@gmail.com>
2011-09-28 12:55:13 -0500

    Using plone.app.theming, the initial theme is registred in GS

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(name='sg.socialtheme',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'plone.app.theming',
       ],
       entry_points="""
       # -*- Entry points: -*-
